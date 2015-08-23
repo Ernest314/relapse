ba1cac287457928cfbe76fbd3c1695c22bce3b3a
Ernest Gu <ernest.gu.3.14@gmail.com>
2015-08-23 15:23:30 -0700

    Added a timer and hooked up its interval
    also changed up some keyboard shortcuts

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -5,9 +5,50 @@
 
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
-	ui(new Ui::MainWindow)
+	ui(new Ui::MainWindow),
+	dir_save(),
+	timer_capture(this),
+	interval_capture(15.00)
 {
 	ui->setupUi(this);
+
+	QObject::connect(this, &MainWindow::updated_interval,
+					 this, &MainWindow::update_interval);
+
+	QObject::connect(ui->radioButton_captureFreq_count,
+					 &QRadioButton::toggled,
+					 this,
+					 &MainWindow::update_interval_count);
+	QObject::connect(ui->comboBox_captureFreq_count_unit,
+					 static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
+					 this,
+					 &MainWindow::update_interval_count);
+	QObject::connect(ui->spinBox_captureFreq_count,
+					 static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
+					 this,
+					 &MainWindow::update_interval_count);
+
+	QObject::connect(ui->radioButton_captureFreq_interval,
+					 &QRadioButton::toggled,
+					 this,
+					 &MainWindow::update_interval_interval);
+	QObject::connect(ui->spinBox_captureFreq_interval_sec,
+					 static_cast<void (QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged),
+					 this,
+					 &MainWindow::update_interval_interval);
+	QObject::connect(ui->spinBox_captureFreq_interval_min,
+					 static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
+					 this,
+					 &MainWindow::update_interval_interval);
+	QObject::connect(ui->spinBox_captureFreq_interval_hr,
+					 static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
+					 this,
+					 &MainWindow::update_interval_interval);
+
+	timer_capture.setInterval(interval_capture);
+
+	ui->spinBox_captureFreq_interval_sec->setValue(15.00);
+	update_interval();
 
 	ui->radioButton_captureFreq_count->setChecked(false);
 	setDisabled_captureFreq_count();
@@ -18,6 +59,45 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
 	delete ui;
+}
+
+void MainWindow::update_interval_count()
+{
+	int unit = 60; // seconds
+	if (ui->comboBox_captureFreq_count_unit->currentText() == "minute") {
+		unit = 60;
+	} else if (ui->comboBox_captureFreq_count_unit->currentText() == "hour") {
+		unit = 60*60;
+	}
+	int count = ui->spinBox_captureFreq_count->value();
+	interval_capture = static_cast<double>(unit) / static_cast<double>(count);
+	emit updated_interval();
+}
+void MainWindow::update_interval_interval()
+{
+	double sec = ui->spinBox_captureFreq_interval_sec->value();
+	int min = ui->spinBox_captureFreq_interval_min->value();
+	int hrs = ui->spinBox_captureFreq_interval_hr->value();
+	interval_capture = sec + 60*min + 60*60*hrs;
+	emit updated_interval();
+}
+void MainWindow::update_interval()
+{
+	timer_capture.setInterval(interval_capture*1000);
+
+	int total_sec = static_cast<int>(round(interval_capture));
+	int sec = static_cast<int>(round(total_sec));
+	int total_min = total_sec - sec;
+	int min = total_min / 60;
+	int total_hrs = total_min - min;
+	int hrs = total_hrs / 60;
+
+	QString time;
+	if (hrs > 0) {
+		time += QString::number(hrs) + ":";
+	}
+	time += QString::number(min) + ":" + QString::number(sec);
+	ui->progressBar_timer->setFormat(time);
 }
 
 void MainWindow::setEnabled_captureFreq_count(bool isEnabled)
@@ -43,4 +123,12 @@ void MainWindow::on_radioButton_captureFreq_interval_toggled(bool checked)
 {
 	setEnabled_captureFreq_interval(checked);
 	setEnabled_captureFreq_count(!checked);
+}
+
+void MainWindow::on_pushButton_folder_clicked()
+{
+	dir_save = QFileDialog::getExistingDirectory(this,
+												 "Save in folder...",
+												 dir_save);
+	ui->lineEdit_folder->setText(dir_save);
 }
--- ui/mainwindow.h
+++ ui/mainwindow.h
@@ -1,6 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <cmath>
+
+#include <QString>
+#include <QTimer>
+
+#include <QFileDialog>
+
 #include <QMainWindow>
 
 
@@ -17,12 +24,25 @@ public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
 
+signals:
+	void updated_interval();
+
 private slots:
+	void update_interval_count();
+	void update_interval_interval();
+	void update_interval();
+
 	void on_radioButton_captureFreq_count_toggled(bool checked);
 	void on_radioButton_captureFreq_interval_toggled(bool checked);
 
+	void on_pushButton_folder_clicked();
+
 private:
 	Ui::MainWindow *ui;
+
+	QString dir_save;
+	QTimer timer_capture;
+	double interval_capture; // seconds
 
 	void setEnabled_captureFreq_count(bool isEnabled=true);
 	void setDisabled_captureFreq_count() { setEnabled_captureFreq_count(false); }
