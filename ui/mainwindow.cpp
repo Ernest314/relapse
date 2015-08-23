a92ca198ac389f598925eb6e58d97948abd9be00
Ernest Gu <ernest.gu.3.14@gmail.com>
2015-08-23 12:45:20 -0700

    Capture freq settings now toggle en-/dis-abled

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -1,14 +1,46 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
+
+	ui->radioButton_captureFreq_count->setChecked(false);
+	setDisabled_captureFreq_count();
+	ui->radioButton_captureFreq_interval->setChecked(true);
+	setEnabled_captureFreq_interval();
 }
 
 MainWindow::~MainWindow()
 {
 	delete ui;
+}
+
+void MainWindow::setEnabled_captureFreq_count(bool isEnabled)
+{
+	ui->comboBox_captureFreq_count_unit->setEnabled(isEnabled);
+	ui->spinBox_captureFreq_count->setEnabled(isEnabled);
+}
+void MainWindow::setEnabled_captureFreq_interval(bool isEnabled)
+{
+	ui->spinBox_captureFreq_interval_hr->setEnabled(isEnabled);
+	ui->label_captureFreq_interval_colon_A->setEnabled(isEnabled);
+	ui->spinBox_captureFreq_interval_min->setEnabled(isEnabled);
+	ui->label_captureFreq_interval_colon_B->setEnabled(isEnabled);
+	ui->spinBox_captureFreq_interval_sec->setEnabled(isEnabled);
+}
+
+void MainWindow::on_radioButton_captureFreq_count_toggled(bool checked)
+{
+	setEnabled_captureFreq_count(checked);
+	setEnabled_captureFreq_interval(!checked);
+}
+void MainWindow::on_radioButton_captureFreq_interval_toggled(bool checked)
+{
+	setEnabled_captureFreq_interval(checked);
+	setEnabled_captureFreq_count(!checked);
 }
--- ui/mainwindow.h
+++ ui/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+
+
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +17,19 @@ public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
 
+private slots:
+	void on_radioButton_captureFreq_count_toggled(bool checked);
+	void on_radioButton_captureFreq_interval_toggled(bool checked);
+
 private:
 	Ui::MainWindow *ui;
+
+	void setEnabled_captureFreq_count(bool isEnabled=true);
+	void setDisabled_captureFreq_count() { setEnabled_captureFreq_count(false); }
+	void setEnabled_captureFreq_interval(bool isEnabled=true);
+	void setDisabled_captureFreq_interval() { setEnabled_captureFreq_interval(false); }
 };
+
+
 
 #endif // MAINWINDOW_H
